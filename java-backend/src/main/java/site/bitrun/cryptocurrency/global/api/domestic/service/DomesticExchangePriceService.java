390a474016fe0c7985969f944aad916bd96006b1
kimdypm <kimdypm@gmail.com>
2026-06-30 12:34:35 +0900

    Remove unused JavaScript files for order and stock functionalities, and delete Docker setup script for database configuration.

--- java-backend/src/main/java/site/bitrun/cryptocurrency/global/api/domestic/service/DomesticExchangePriceService.java
+++
@@ -1,8 +0,0 @@
-package site.bitrun.cryptocurrency.global.api.domestic.service;
-
-import site.bitrun.cryptocurrency.global.api.domestic.dto.DomesticExchangePriceResponseDto;
-
-public interface DomesticExchangePriceService {
-
-    DomesticExchangePriceResponseDto getDomesticPrices(String marketCode);
-}
--- java-backend/src/main/java/site/bitrun/cryptocurrency/session/SessionConst.java
+++
@@ -1,5 +0,0 @@
-package site.bitrun.cryptocurrency.session;
-
-public abstract class SessionConst {
-    public static final String LOGIN_MEMBER = "loginMember";
-}
